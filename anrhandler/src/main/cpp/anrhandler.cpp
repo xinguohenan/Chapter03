d7cae547a477bf5bafa8a8aecf29d485bab6d9ad
FAREAST\xishui <xishui@microsoft.com>
2019-03-14 10:19:06 +0800

    Add anrhandler.

--- anrhandler/src/main/cpp/anrhandler.cpp
+++ anrhandler/src/main/cpp/anrhandler.cpp
@@ -6,33 +6,36 @@
 #include <string.h>
 
 static JavaVM* g_JVM; // JavaVM is valid for all threads, so just save it globally
+static jobject anrMonitorObj;
 
 static void anr_sig_handler(int sig, siginfo_t *siginfo, void *context) {
     JNIEnv *env = 0;
     g_JVM->AttachCurrentThread(&env, 0);
 
     jclass clazz = env->FindClass("com/xishui/anrhandler/ANRMonitor");
-    jmethodID methodId = env->GetStaticMethodID(clazz, "reportANR", "()V");
-    env->CallStaticVoidMethod(clazz, methodId);
+    jmethodID methodId = env->GetMethodID(clazz, "reportANR", "()V");
+    env->CallVoidMethod(anrMonitorObj, methodId);
 }
 
-static void handle_anr() {
+static void register_anr_handler() {
     struct sigaction act;
     memset (&act, '\0', sizeof(act));
     /* Use the sa_sigaction field because the handles has two additional parameters */
     act.sa_sigaction = &anr_sig_handler;
     /* The SA_SIGINFO flag tells sigaction() to use the sa_sigaction field, not sa_handler. */
     act.sa_flags = SA_SIGINFO;
-    sigaction(SIGQUIT, &act, NULL);
+    int result = sigaction(SIGQUIT, &act, NULL);
+    int result2 = result;
 }
 
 extern "C" {
 // https://developer.android.com/training/articles/perf-jni.html
 // Until Android 2.0 (Eclair), the '$' character was not properly converted to "_00024" during searches for method names.
 // Working around this requires using explicit registration or moving the native methods out of inner classes.
-void Java_com_microsoft_bing_dss_process_BaseAppHost_setUpANRHandler(
+JNIEXPORT void Java_com_xishui_anrhandler_ANRMonitor_setUpANRHandler(
         JNIEnv* env, jobject obj) {
     env->GetJavaVM(&g_JVM);
-    handle_anr();
+    register_anr_handler();
+    anrMonitorObj = env->NewGlobalRef(obj);
 }
 }
--- anrhandler/src/main/java/com/xishui/anrhandler/ANRMonitor.java
+++ anrhandler/src/main/java/com/xishui/anrhandler/ANRMonitor.java
@@ -1,71 +1,47 @@
 package com.xishui.anrhandler;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Looper;
 import android.util.Log;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.concurrent.TimeUnit;
-
 public class ANRMonitor {
     private static final String LOG_TAG = ANRMonitor.class.getSimpleName();
 
     public native String dumpForSigQuit();
+    public native void setUpANRHandler();
+
+    static {
+        System.loadLibrary("anrhandler");
+    }
 
     public interface ANRHandler {
         void onApplicationNotResponding();
     }
 
-    private static final String KEY_THREAD_STACK = "KeyThreadStack";
-
     private static CpuSampler sCpuSampler = new CpuSampler(1000);
 
-    private static ANRHandler sANRHandler = new ANRHandler() {
-        @Override
-        public void onApplicationNotResponding() {
-//            if (BaseUtils.getProcessName().equalsIgnoreCase(BuildConfig.MAIN_PROCESS_NAME)) {
-//                if (s_tcpClient != null) {
-//                    s_tcpClient.setContent(getAllThreadStackTrace() + " " + INSTANCE.dumpForSigQuit());
-//                }
-//            } else {
-//                Analytics.logEvent(false, AnalyticsEvent.PERFORMANCE, new BasicNameValuePair[] {
-//                        new BasicNameValuePair(AnalyticsConstants.ACTION_NAME, "ANR"),
-//                        new BasicNameValuePair(KEY_THREAD_STACK, getAllThreadStackTrace()),
-//                        new BasicNameValuePair("CpuRate", sCpuSampler.getCpuRateInfo()),
-//                        new BasicNameValuePair(AnalyticsConstants.ACTION_VALUE, BaseUtils.getProcessName())});
-//            }
+    private ANRHandler mDefaultANRHandler;
+//    public static ANRMonitor INSTANCE = new ANRMonitor(sDefaultANRHandler);
+
+    private ANRHandler mANRHandler;
+    private boolean mIsRunning = false;
+
+    public void reportANR() {
+        if (mANRHandler != null) {
+            mANRHandler.onApplicationNotResponding();
+        } else {
+            mDefaultANRHandler.onApplicationNotResponding();
         }
-    };
-    public static ANRMonitor INSTANCE = new ANRMonitor(sANRHandler);
-
-    private static final int SOCKET_PORT = 23563;
-    private ANRHandler _anrHandler;
-    private Thread _thread = null;
-    public static final long ANR_THRESHOLD_MILLSECONDS = 6000;
-    private boolean _isRunning = false;
-    private static TCPClient s_tcpClient;
-
-    public static void reportANR() {
-        sANRHandler.onApplicationNotResponding();
     }
 
-    public static void init() {
-        sCpuSampler.start();
-//        if (BaseUtils.getProcessName().equalsIgnoreCase(BuildConfig.KEY_PROCESS_NAME)) {
-//            initKeyProcessSocketServer();
-//        } else if (BaseUtils.getProcessName().equalsIgnoreCase(BuildConfig.MAIN_PROCESS_NAME)) {
-//            s_tcpClient = initMainProcessSocketClient();
-//        }
+    public void init() {
+        // ToDO: Should we start CPU Sampler in production build or only in debug build?
+        //sCpuSampler.start();
 
-        INSTANCE.start();
+        setUpANRHandler();
+
+        start();
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(Intent.ACTION_SCREEN_OFF);
         intentFilter.addAction(Intent.ACTION_SCREEN_ON);
@@ -91,6 +67,13 @@ public class ANRMonitor {
 //                }
 //            }
 //        }, intentFilter);
+
+        mDefaultANRHandler = new ANRHandler() {
+            @Override
+            public void onApplicationNotResponding() {
+                Log.e(LOG_TAG, "ANR signal is received! : " + getAllThreadStackTrace() + " " + dumpForSigQuit());
+            }
+        };
     }
 
     public static String getAllThreadStackTrace() {
@@ -105,89 +88,17 @@ public class ANRMonitor {
     }
 
     public ANRMonitor(ANRHandler anrHandler) {
-        _anrHandler = anrHandler;
+        mANRHandler = anrHandler;
+        init();
     }
 
     public synchronized void start() {
-        if (!_isRunning) {
-            _isRunning = true;
+        if (!mIsRunning) {
+            mIsRunning = true;
         }
     }
 
     public synchronized void stop() {
-        _isRunning = false;
-    }
-
-    private static void initKeyProcessSocketServer() {
-        new Thread(new TCPServer()).start();
-    }
-
-    private static TCPClient initMainProcessSocketClient() {
-        TCPClient tcpClient = new TCPClient();
-        new Thread(tcpClient).start();
-        return tcpClient;
-    }
-
-    static class TCPServer implements Runnable {
-        @Override
-        public void run() {
-            try {
-                ServerSocket ss = new ServerSocket(SOCKET_PORT);
-                while(true) {
-                    Socket socket = ss.accept();
-                    BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    StringBuilder sb = new StringBuilder();
-                    String line;
-                    while ((line = br.readLine()) != null) {
-                        sb.append(line);
-                        sb.append("\r\n");
-                    }
-
-                    String mainThreadStackTrace = sb.toString();
-                    String keyThreadStackStrace = getAllThreadStackTrace();
-
-//                    Analytics.logEvent(false, AnalyticsEvent.PERFORMANCE, new BasicNameValuePair[] {
-//                            new BasicNameValuePair(AnalyticsConstants.ACTION_NAME, "ANR"),
-//                            new BasicNameValuePair("MainThreadStack", mainThreadStackTrace),
-//                            new BasicNameValuePair(KEY_THREAD_STACK, keyThreadStackStrace),
-//                            new BasicNameValuePair("CpuRate", sCpuSampler.getCpuRateInfo()),
-//                            new BasicNameValuePair(AnalyticsConstants.ACTION_VALUE, BaseUtils.getProcessName()),
-//                            new BasicNameValuePair(AnalyticsConstants.STATE_NAME, isLauncherEnabled)});
-
-                    Log.d(LOG_TAG, "main" + mainThreadStackTrace);
-                    Log.d(LOG_TAG, "key" + keyThreadStackStrace);
-                    Log.d(LOG_TAG, "cpurate" + sCpuSampler.getCpuRateInfo());
-                    socket.close();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    static class TCPClient implements Runnable {
-        private String _content;
-
-        @Override
-        public void run() {
-            try {
-                while (_content == null) {
-                    Thread.sleep(1000);
-                }
-                Socket socket = new Socket("localhost", SOCKET_PORT);
-                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-                bw.write(_content);
-                bw.flush();
-                _content = null;
-                //socket.close();
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        public void setContent(String content) {
-            _content = content;
-        }
+        mIsRunning = false;
     }
 }
